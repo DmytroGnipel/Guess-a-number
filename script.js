f6614f22cad5810ee34a72be965bc789d4262ada
Dmytro Hnipel <dmitrognipel@gmail.com>
2023-02-15 01:33:45 +0200

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let compNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1
+console.log(compNumber)
+let input = document.querySelector('input')
+let para = document.querySelector('p')
+
+input.addEventListener('blur', () => {
+    if (input.value > 100 || input.value < 1) para.textContent = 'Your number must be between from 1 to 100 (both including)'
+    else {
+    if (input.value < compNumber) para.textContent = 'Input greater number'
+    else if (input.value > compNumber) para.textContent = 'Input lesser number'
+    else if (input.value == compNumber) para.textContent = `Yes! You are champion! Computer have guessed number ${compNumber}`   
+}
+})
